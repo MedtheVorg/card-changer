af2e44b514a93f2885b268c58d2d02451480bd60
soka1998 <soukainaharifi18@gmail.com>
2023-12-15 11:01:01 +0100

    add logic on button 3

--- script.js
+++ script.js
@@ -3,6 +3,25 @@ const card = document.querySelector(".card");
 const cardTitle = document.querySelector(".card h1");
 const cardContent = document.querySelector(".card .content");
 
+
+// script.js
+
+document.addEventListener("DOMContentLoaded", function () {
+    // Get the button with class "btn-3"
+    var btn3 = document.getElementById("btn3");
+  
+    // Add a click event listener to the button
+    btn3.addEventListener("click", function () {
+      // Your logic for button with class "btn-3" goes here
+      alert("Button 3 clicked!"); // Example: Display an alert
+  
+      // You can also modify the content of the card or perform other actions
+      var cardContent = document.querySelector(".content");
+      cardContent.textContent = "Button 3 clicked!";
+    });
+  });
+  
+
 // buttons
 const button_one = document.querySelectorAll(".controllers .btn")[0];
 const button_two = document.querySelectorAll(".controllers .btn")[1];
