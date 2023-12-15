4d55f26116edd1e2ca24e4b54dd8885b9a48b080
MEDPC <mohamedlem216@gmail.com>
2023-12-15 10:23:52 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+const container = document.querySelector(".container");
+const card = document.querySelector(".card");
+const cardTitle = document.querySelector(".card h1");
+const cardContent = document.querySelector(".card .content");
+
+// buttons
+const button_one = document.querySelectorAll(".controllers .btn")[0];
+const button_two = document.querySelectorAll(".controllers .btn")[1];
+const button_three = document.querySelectorAll(".controllers .btn")[2];
+const button_four = document.querySelectorAll(".controllers .btn")[3];
+const button_five = document.querySelectorAll(".controllers .btn")[4];
+
+// template first button Code
+
+// write yout button event listener here
+// you are free to change the card design as you like
+// ------------------------------
