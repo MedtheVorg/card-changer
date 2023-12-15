5a9b9bd634399cbd887e840747ee94536bf4eb4c
laitouss-yassine <yassinelaitouss2380@gmail.com>
2023-12-15 11:47:11 +0100

    yassineLaitouss/added logic to card and button

--- script.js
+++ script.js
@@ -15,7 +15,32 @@ const button_five = document.querySelectorAll(".controllers .btn")[4];
 // write yout button event listener here
 // you are free to change the card design as you like
 // ------------------------------
+button_four.addEventListener("mouseover", function() {
+    // Add hover style
+    button_four.style.backgroundColor = "#23c483";
+    button_four.style.boxShadow = "0px 15px 20px rgba(46, 229, 157, 0.4)";
+    button_four.style.color = "#fff";
+    button_four.style.transform = "translateY(-7px)";
+});
 
+button_four.addEventListener("mouseout", function() {
+    // Remove hover style
+    button_four.style.backgroundColor = "";  // Reset to default
+    button_four.style.boxShadow = "";  // Reset to default
+    button_four.style.color = "";  // Reset to default
+    button_four.style.transform = "";  // Reset to default
+});
+
+button_four.addEventListener("mousedown", function() {
+    // Add active style
+    button_four.style.transform = "translateY(-1px)";
+});
+
+button_four.addEventListener("mouseup", function() {
+    // Remove active style
+    button_four.style.transform = "";  // Reset to default
+});
+button_four.style.cssText = 'padding: 1.3em 3em;font-size: 12px;text-transform: uppercase;letter-spacing: 2.5px;font-weight: 500;color: #000;background-color: #fff;border: none;border-radius: 45px;box-shadow: 0px 8px 15px rgba(0, 0, 0, 0.1);transition: all 0.3s ease 0s;cursor: pointer;outline: none;'
 button_four.addEventListener('click', () => {
     card.style.cssText  = 'background-color: #23c483;box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4); width: 400px; height: 400px; padding: 10px 20px; border-radius: 45px;';
     cardTitle.textContent = 'Welcome Abtal Tech';
